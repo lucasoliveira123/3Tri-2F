4f564197e819628967df895bf9a8e0b8d4dcc617
TABACARIA 2K <106823609+lucasoliveira123@users.noreply.github.com>
2024-11-07 12:27:30 -0300

    07/11

---
+++ assets/main.js
@@ -0,0 +1,21 @@
+function criaCartao(tema,pergunta,resposta){
+    const container = document.getElementById('container');
+    let cartao = document.createElement('article');
+    cartao.className = 'cartao';
+    cartao.innerHTML = `
+    <div class="cartao-conteudo">
+<h3>${tema}</h3>
+<div class="cartao-conteudo-pergunta">
+   ${pergunta}
+</div>
+<div class="cartao-conteudo-resposta">
+    ${resposta}
+</div>
+</div>`
+
+container.appendChild(cartao)
+
+
+}
+criaCartao('historia','em que ano iniciou-se a gurra do vietna','Em 1955')
+criaCartao('portugues','oque e paroxitona','uma paroxitona e uma palavra cuja silaba tonica e penultima')
